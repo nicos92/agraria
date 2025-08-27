43e77cad62e3fbf69c39bca85fb9db98ad4dd14c
nicos92 <nicolassandoval140692@gmail.com>
2025-08-27 17:20:23 -0300

    segundo cambio de color a azul.

--- Agraria.Login/Form1.Designer.cs
+++ Agraria.Login/Form1.Designer.cs
@@ -30,7 +30,10 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
         TLPFondo = new TableLayoutPanel();
+        TLPLogo = new TableLayoutPanel();
+        Logo = new PictureBox();
         TLPInicio = new TableLayoutPanel();
         BtnIngresar = new Button();
         LblInicioError = new Label();
@@ -38,22 +41,22 @@ partial class Form1
         label3 = new Label();
         TxtUsuario = new TextBox();
         LblUsuario = new Label();
-        label1 = new Label();
-        Logo = new PictureBox();
+        LblTitulo = new Label();
         LblOlvide = new LinkLabel();
         TLPFondo.SuspendLayout();
-        TLPInicio.SuspendLayout();
+        TLPLogo.SuspendLayout();
         ((System.ComponentModel.ISupportInitialize)Logo).BeginInit();
+        TLPInicio.SuspendLayout();
         SuspendLayout();
         // 
         // TLPFondo
         // 
-        TLPFondo.BackColor = Color.FromArgb(235, 231, 235);
+        TLPFondo.BackColor = Color.FromArgb(203, 230, 255);
         TLPFondo.ColumnCount = 2;
         TLPFondo.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 39.3589745F));
         TLPFondo.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 60.6410255F));
+        TLPFondo.Controls.Add(TLPLogo, 0, 0);
         TLPFondo.Controls.Add(TLPInicio, 1, 0);
-        TLPFondo.Controls.Add(Logo, 0, 0);
         TLPFondo.Dock = DockStyle.Fill;
         TLPFondo.Location = new Point(0, 0);
         TLPFondo.Name = "TLPFondo";
@@ -62,9 +65,34 @@ partial class Form1
         TLPFondo.Size = new Size(780, 457);
         TLPFondo.TabIndex = 0;
         // 
+        // TLPLogo
+        // 
+        TLPLogo.BackColor = Color.FromArgb(7, 100, 147);
+        TLPLogo.ColumnCount = 1;
+        TLPLogo.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        TLPLogo.Controls.Add(Logo, 0, 0);
+        TLPLogo.Dock = DockStyle.Fill;
+        TLPLogo.Location = new Point(3, 3);
+        TLPLogo.Name = "TLPLogo";
+        TLPLogo.RowCount = 1;
+        TLPLogo.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+        TLPLogo.Size = new Size(301, 451);
+        TLPLogo.TabIndex = 9;
+        // 
+        // Logo
+        // 
+        Logo.Anchor = AnchorStyles.None;
+        Logo.Image = Properties.Resources.EA_C_256_;
+        Logo.Location = new Point(22, 97);
+        Logo.Name = "Logo";
+        Logo.Size = new Size(256, 256);
+        Logo.SizeMode = PictureBoxSizeMode.Zoom;
+        Logo.TabIndex = 1;
+        Logo.TabStop = false;
+        // 
         // TLPInicio
         // 
-        TLPInicio.BackColor = Color.FromArgb(241, 237, 240);
+        TLPInicio.BackColor = Color.FromArgb(7, 100, 147);
         TLPInicio.ColumnCount = 3;
         TLPInicio.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
         TLPInicio.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 60F));
@@ -75,10 +103,10 @@ partial class Form1
         TLPInicio.Controls.Add(label3, 1, 4);
         TLPInicio.Controls.Add(TxtUsuario, 1, 3);
         TLPInicio.Controls.Add(LblUsuario, 1, 2);
-        TLPInicio.Controls.Add(label1, 1, 1);
+        TLPInicio.Controls.Add(LblTitulo, 1, 1);
         TLPInicio.Controls.Add(LblOlvide, 1, 8);
         TLPInicio.Dock = DockStyle.Fill;
-        TLPInicio.ForeColor = Color.FromArgb(28, 27, 30);
+        TLPInicio.ForeColor = Color.FromArgb(255, 255, 255);
         TLPInicio.Location = new Point(310, 3);
         TLPInicio.Name = "TLPInicio";
         TLPInicio.RowCount = 10;
@@ -98,11 +126,12 @@ partial class Form1
         // BtnIngresar
         // 
         BtnIngresar.Anchor = AnchorStyles.Top;
-        BtnIngresar.BackColor = Color.FromArgb(92, 87, 151);
+        BtnIngresar.BackColor = Color.FromArgb(203, 230, 255);
         BtnIngresar.Cursor = Cursors.Hand;
+        BtnIngresar.FlatAppearance.BorderColor = Color.White;
         BtnIngresar.FlatStyle = FlatStyle.Flat;
         BtnIngresar.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-        BtnIngresar.ForeColor = Color.FromArgb(255, 255, 255);
+        BtnIngresar.ForeColor = Color.FromArgb(0, 75, 113);
         BtnIngresar.Location = new Point(169, 290);
         BtnIngresar.Name = "BtnIngresar";
         BtnIngresar.Size = new Size(128, 48);
@@ -112,17 +141,15 @@ partial class Form1
         // 
         // LblInicioError
         // 
-        LblInicioError.ForeColor = AppColorPalette.Light.Error;
-        
         LblInicioError.Anchor = AnchorStyles.None;
         LblInicioError.AutoSize = true;
-        LblInicioError.ForeColor = Color.IndianRed;
+        LblInicioError.BackColor = Color.FromArgb(255, 218, 214);
+        LblInicioError.ForeColor = Color.FromArgb(65, 0, 2);
         LblInicioError.Location = new Point(114, 257);
         LblInicioError.Name = "LblInicioError";
         LblInicioError.Size = new Size(237, 21);
         LblInicioError.TabIndex = 7;
         LblInicioError.Text = "Usuario o contraseña incorrectas";
-        LblInicioError.Visible = false;
         // 
         // TxtContrasenia
         // 
@@ -162,36 +189,25 @@ partial class Form1
         LblUsuario.TabIndex = 1;
         LblUsuario.Text = "Usuario:";
         // 
-        // label1
+        // LblTitulo
         // 
-        label1.Anchor = AnchorStyles.None;
-        label1.AutoSize = true;
-        label1.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        label1.Location = new Point(113, 23);
-        label1.Name = "label1";
-        label1.Size = new Size(240, 45);
-        label1.TabIndex = 0;
-        label1.Text = "inicio de sesión";
-        label1.Click += label1_Click;
-        // 
-        // Logo
-        // 
-        Logo.Anchor = AnchorStyles.None;
-        Logo.Image = Properties.Resources.EA_C_256_;
-        Logo.Location = new Point(25, 100);
-        Logo.Name = "Logo";
-        Logo.Size = new Size(256, 256);
-        Logo.SizeMode = PictureBoxSizeMode.Zoom;
-        Logo.TabIndex = 1;
-        Logo.TabStop = false;
+        LblTitulo.Anchor = AnchorStyles.None;
+        LblTitulo.AutoSize = true;
+        LblTitulo.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+        LblTitulo.ForeColor = AppColorPalette.Light.OnPrimary;
+        LblTitulo.Location = new Point(108, 23);
+        LblTitulo.Name = "LblTitulo";
+        LblTitulo.Size = new Size(249, 45);
+        LblTitulo.TabIndex = 0;
+        LblTitulo.Text = "inicio de sesión";
+        LblTitulo.Click += label1_Click;
         // 
         // LblOlvide
         // 
-        LblOlvide.LinkColor = AppColorPalette.Light.Tertiary;
-        LblOlvide.VisitedLinkColor = AppColorPalette.Light.TertiaryContainer;
-        LblOlvide.ActiveLinkColor = AppColorPalette.Light.Primary;
+        LblOlvide.ActiveLinkColor = Color.FromArgb(92, 87, 151);
         LblOlvide.Anchor = AnchorStyles.None;
         LblOlvide.AutoSize = true;
+        LblOlvide.LinkColor = Color.FromArgb(255, 255, 255);
         LblOlvide.Location = new Point(139, 381);
         LblOlvide.Name = "LblOlvide";
         LblOlvide.Size = new Size(187, 42);
@@ -199,6 +215,7 @@ partial class Form1
         LblOlvide.TabStop = true;
         LblOlvide.Text = "¿Olvidaste tu contraseña?\r\nhaz click aquí";
         LblOlvide.TextAlign = ContentAlignment.MiddleCenter;
+        LblOlvide.VisitedLinkColor = Color.FromArgb(255, 216, 232);
         // 
         // Form1
         // 
@@ -207,13 +224,17 @@ partial class Form1
         Controls.Add(TLPFondo);
         Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
         FormBorderStyle = FormBorderStyle.Fixed3D;
+        Icon = (Icon)resources.GetObject("$this.Icon");
+        MaximizeBox = false;
+        MinimizeBox = false;
         Name = "Form1";
         StartPosition = FormStartPosition.CenterScreen;
         Text = "Escuela Agraria";
         TLPFondo.ResumeLayout(false);
+        TLPLogo.ResumeLayout(false);
+        ((System.ComponentModel.ISupportInitialize)Logo).EndInit();
         TLPInicio.ResumeLayout(false);
         TLPInicio.PerformLayout();
-        ((System.ComponentModel.ISupportInitialize)Logo).EndInit();
         ResumeLayout(false);
     }
 
@@ -221,7 +242,7 @@ partial class Form1
 
     private TableLayoutPanel TLPFondo;
     private TableLayoutPanel TLPInicio;
-    private Label label1;
+    private Label LblTitulo;
     private Label LblUsuario;
     private Label label3;
     private TextBox TxtUsuario;
@@ -230,4 +251,5 @@ partial class Form1
     private Label LblInicioError;
     private PictureBox Logo;
     private LinkLabel LblOlvide;
+    private TableLayoutPanel TLPLogo;
 }
