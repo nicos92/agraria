e0f868ec619b361ae3c2517cb813aa747375e094
Nicolas sandoval <nicolassandoval140692@gmail.com>
2025-10-21 19:35:55 -0300

    feat: mejora la vista de las impresiones bajando el padding de los talbe data

--- Agraria.Repositorio/BaseRepositorio.cs
+++ Agraria.Repositorio/BaseRepositorio.cs
@@ -24,12 +24,12 @@ namespace Agraria.Repositorio
             //cadenaConexion = "Server=NICOS\\SQLEXPRESS;Database=Agraria;Trusted_Connection=True;TrustServerCertificate=True;";
 
             // CONEXION PARA T440
-            //cadenaConexion = "Server=NicoS92T440;Database=Agraria;Trusted_Connection=True;TrustServerCertificate=True;";
+            cadenaConexion = "Server=NicoS92T440;Database=Agraria;Trusted_Connection=True;TrustServerCertificate=True;";
             // CONEXION PARA i9
             //cadenaConexion = @"Data Source = (localdb)\MSSQLLocalDB; Integrated Security = True; Persist Security Info = False; Pooling = False; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = True; Application Name = 'SQL Server Management Studio'; Command Timeout = 30; Initial Catalog = Agraria";
             //cadenaConexion = ConfigurationManager.ConnectionStrings["SqlServerAgrariaLocal"].ConnectionString;
             // CONEXION PARA DOCKER CASA
-            cadenaConexion = @"Server=127.0.0.1,14333;Database=Agraria;User Id=sa;Password=Pass2025@;TrustServerCertificate=True;";
+            //cadenaConexion = @"Server=127.0.0.1,14333;Database=Agraria;User Id=sa;Password=Pass2025@;TrustServerCertificate=True;";
 
 
         }
